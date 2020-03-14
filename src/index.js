0fc57229cc826e22cc28e1adace15b53c676c23a
Dmitriy Smolin <45199267+DmitriySmolin@users.noreply.github.com>
2020-03-14 19:31:38 +0400

    feat: add solutions for task "human-readable-number"

--- src/index.js
+++ src/index.js
@@ -1,3 +1,56 @@
-module.exports = function toReadable (number) {
-  
+module.exports = function toReadable(number) {
+    if (number === 0) return "zero";
+
+    const tokens = {
+        1: "one",
+        2: "two",
+        3: "three",
+        4: "four",
+        5: "five",
+        6: "six",
+        7: "seven",
+        8: "eight",
+        9: "nine",
+        10: "ten",
+        11: "eleven",
+        12: "twelve",
+        13: "thirteen",
+        14: "fourteen",
+        15: "fifteen",
+        16: "sixteen",
+        17: "seventeen",
+        18: "eighteen",
+        19: "nineteen",
+        20: "twenty",
+        30: "thirty",
+        40: "forty",
+        50: "fifty",
+        60: "sixty",
+        70: "seventy",
+        80: "eighty",
+        90: "ninety"
+    };
+
+    let str = String(number);
+    let result = "";
+    let hundreds = "";
+
+    if (str.length === 3) {
+        hundreds = `${tokens[str[0]]} hundred`;
+        str = str.slice(1);
+    }
+
+    if (!tokens[str]) {
+        const dozens = tokens[str[0]] ? `${tokens[str[0] * 10]}` : "";
+        const units = tokens[str[1]];
+
+        if (dozens === "") result = `${hundreds} ${units}`;
+        else result = `${hundreds} ${dozens} ${units}`;
+    }
+
+    if (tokens[str]) result = `${hundreds} ${tokens[str]}`;
+
+    if (str === "00") return hundreds;
+
+    return result.trim();
 }
